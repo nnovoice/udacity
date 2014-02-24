cf6e5c68af81f3c1b5e9e0b97063ea324cdf974c
n.raghavendran <n.raghavendran@gmail.com>
2014-02-24 18:20:53 +0530

    Another attempt. Bad.

--- intro-to-cs/unit-6-how-to-have-infinite-power/homework/quicksort.py
+++ intro-to-cs/unit-6-how-to-have-infinite-power/homework/quicksort.py
@@ -5,43 +5,37 @@ def partition(l):
     print "partition called on ", l, " with len= ", len(l)
     count = len(l)
     pivot = l[0]
-    i = 0
-    j = count - 1
-    while (i < j):
-        left = i
-        while left <= j:
-            if (l[left] > pivot):
-                break
+    left = 0
+    right = count - 1
+    print "ENTRY: left= ", left, " leftVal= ", l[left], " right= ", right, " rightVal= ", l[right]
+    while (left < right):
+        while (l[left] < pivot and left < count):
             left += 1
 
-        right = j
-        while (right >= i):
-            if (l[right] < pivot):
-                break
+        while (l[right] > pivot and right >= 0):
             right -= 1
 
         if (left < right):
-            print "left= ", left, " leftVal= ", l[left], " right= ", right, " rightVal= ", l[right]
+            print "SWAP: left= ", left, " leftVal= ", l[left], " right= ", right, " rightVal= ", l[right]
             l[left], l[right] = l[right], l[left]
-        else:
-            break
 
-        i = left
-        j = right
+    print "OUTSIDE: left= ", left, " leftVal= ", l[left], " right= ", right, " rightVal= ", l[right]
 
-    print " right= ", right, " rightVal= ", l[right]
-    print "left= ", left, " leftVal= ", l[left]
-    l[0],l[left] = l[left],l[0]
-    print l
-    return left
+    if (left < count):
+        l[0],l[left] = l[left],l[0]
+        print l
+        return left
+
+    return -1
 
 def quicksort(l):
     if len(l) <= 1:
         return
     
     new_pivot_index = partition(l)
-    quicksort(l[0:new_pivot_index])
-    quicksort(l[new_pivot_index:])
+    if (new_pivot_index > 0):
+        quicksort(l[0:new_pivot_index])
+        quicksort(l[new_pivot_index:])
 
 print arr
 quicksort(arr)
