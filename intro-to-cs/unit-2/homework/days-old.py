46193c37e6cbf623a104eccf2901db1060c8fe75
n.raghavendran <n.raghavendran@gmail.com>
2014-03-26 19:37:23 +0530

    refactored code. Looks simpler and easier to understand.

--- intro-to-cs/unit-2/homework/days-old.py
+++ intro-to-cs/unit-2/homework/days-old.py
@@ -12,55 +12,36 @@ debug = False
 days_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def isLeapYear(year):
-    if ((year % 4) == 0):
-        if ((year % 100) == 0):
-            if ((year % 400) != 0):
-                return False
-            else:
-                return True
+    if ((year % 4) != 0):
+        return False
+    
+    if ((year % 100) == 0):
+        if ((year % 400) != 0):
+            return False
         else:
             return True
-    return False
+    
+    return True
         
 def getNextDay(year, month, day):
-    next_year = year
-    next_month = month
-    next_day = day
-    
     num_days = days_in_month[month]
     if debug == True:
         print 'month=', month, 'num_days=', num_days
-    if (day == num_days):
-        if (month == 2 and isLeapYear(year) == True):
-            if debug == True:
-                print 'month=', month, 'leap year=', True
-            next_day = 29
-        else:
-            if (month == 12):
-                next_day = 1
-                next_month = 1
-                next_year += 1
-            else:
-                next_day = 1
-                next_month = next_month + 1
-    else:
-        if (month == 2 and isLeapYear(year) == True and day > num_days):
-            next_day = 1
-            next_month += 1
-            if debug == True:
-                print 'Month is Feb in a leap year and days are > 28, so next day=', next_year, next_month, next_day
-        else:
-            next_day += 1
+        
+    if (day < num_days):
+        return (year, month, day + 1)
+    
+    if (month == 2 and isLeapYear(year) == True):
+        if (day == num_days):
+            return (year, month, day + 1)
 
-    if debug == True:
-        print 'Next date=', next_year, next_month, next_day
-    return (next_year, next_month, next_day)
+    if (month == 12):
+        return (year + 1, 1, 1)
+
+    return (year, month + 1, 1)
     
 
 def daysBetweenDates(year1, month1, day1, year2, month2, day2):
-    ##
-    # Your code here.
-    ##
     numDays = 0
     nextDate = (year1, month1, day1)
     while nextDate != (year2, month2, day2):
@@ -94,6 +75,7 @@ print 'Next day of ', (2012, 2, 29), getNextDay(2012, 2, 29), 'result=', getNext
 print 'Next day of ', (2012, 2, 25), getNextDay(2012, 2, 25), 'result=', getNextDay(2012, 2, 25) == (2012, 2, 26)
 print 'Next day of ', (2013, 2, 28), getNextDay(2013, 2, 28), 'result=', getNextDay(2013, 2, 28) == (2013, 3, 1)
 print 'Next day of ', (2000, 2, 28), getNextDay(2000, 2, 28), 'result=', getNextDay(2000, 2, 28) == (2000, 2, 29)
+print 'Next day of ', (1900, 1, 1), getNextDay(1900, 1, 1), 'result=', getNextDay(1900, 1, 1) == (1900, 1, 2)
 
 test()
 #print daysBetweenDates(2012,1,1,2012,2,28)
