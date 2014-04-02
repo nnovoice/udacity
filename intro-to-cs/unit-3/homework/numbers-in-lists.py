aa9660e21cec7aeec0dc099c83ad25aea55cc4af
n.raghavendran <n.raghavendran@gmail.com>
2014-04-02 09:25:15 +0530

    Got this right. Before inserting into the outer list check if the inner list is non-empty, if so, append that first and then the outer list append.

--- intro-to-cs/unit-3/homework/numbers-in-lists.py
+++ intro-to-cs/unit-3/homework/numbers-in-lists.py
@@ -27,6 +27,11 @@ def numbers_in_lists(string):
     for c in string:
         digit = int(c)
         if (digit > outer_list_max_digit):
+            if (len(inner_list) > 0):
+                outer_list.append(inner_list)
+                inner_list = []
+                inner_list_max_digit = 0
+                
             outer_list.append(digit)
             outer_list_max_digit = digit
         else:
@@ -41,7 +46,6 @@ def numbers_in_lists(string):
     if (len(inner_list) > 0):
         outer_list.append(inner_list)
 
-    print 'outer list=', outer_list
     return outer_list
         
 
@@ -51,10 +55,19 @@ result = [5,[4,3],9,[8,7]]
 print repr(string), 'got=', numbers_in_lists(string), 'expected=', result, numbers_in_lists(string) == result
 string= '987654321'
 result = [9,[8,7,6,5,4,3,2,1]]
-print repr(string), numbers_in_lists(string) == result
+print repr(string), 'got=', numbers_in_lists(string), 'expected=', result, numbers_in_lists(string) == result
 string = '455532123266'
 result = [4, 5, [5, 5, 3, 2, 1, 2, 3, 2], 6, [6]]
-print repr(string), numbers_in_lists(string) == result
+print repr(string), 'got=', numbers_in_lists(string), 'expected=', result, numbers_in_lists(string) == result
 string = '123456789'
 result = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print repr(string), numbers_in_lists(string) == result
+print repr(string), 'got=', numbers_in_lists(string), 'expected=', result, numbers_in_lists(string) == result
+string = '5465768798'
+result = [5,[4],6,[5],7,[6],8,[7],9,[8]]
+print repr(string), 'got=', numbers_in_lists(string), 'expected=', result, numbers_in_lists(string) == result
+string = '5432154321'
+result = [5,[4,3,2,1],[5,4,3,2,1]]
+print repr(string), 'got=', numbers_in_lists(string), 'expected=', result, numbers_in_lists(string) == result
+string = '6767545489893232'
+result = [6,7,[6],[7,5,4,5,4],8,9,[8],[9,3,2,3,2]]
+print repr(string), 'got=', numbers_in_lists(string), 'expected=', result, numbers_in_lists(string) == result
